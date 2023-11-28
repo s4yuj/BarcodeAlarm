ed42659dbded03df64e88f424c34106def518038
s4yuj <sayujsethi@gmail.com>
2023-11-28 15:36:49 -0800

    Initial Commit

---
+++ Barcode Alarm/Barcode_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_AlarmApp.swift
+//  Barcode Alarm
+//
+//  Created by Sayuj Sethi on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barcode Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Barcode Alarm
+//
+//  Created by Sayuj Sethi on 28/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
